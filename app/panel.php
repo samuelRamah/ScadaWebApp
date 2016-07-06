bb0adf9cd01573fcd276a4a35364fa64b94014aa
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-06 19:15:42 +0300

    UI improvement : Input for sensors data, Slider for actuator command, Switch for actuator command

--- app/panel.php
+++ app/panel.php
@@ -90,7 +90,7 @@ function createPanel($node, $i){
 	$classButton = array('btn btn-default','btn btn-success','btn btn-info','btn btn-warning','btn btn-primary','btn btn-danger');
 	$backgroundColor = array('grey','#449d44','#31b0d5','#f0ad4e','#286090','#c9302c')
 ?>
-	<div class="col-sm-4 col-md-4 col-lg-3">
+	<div class="cols-xs-6 col-sm-6 col-md-4 col-lg-3">
 		<div class= <?php echo "\"node " . $classPanel[$i % 6]."\""; ?> id=<?php echo "\"node-". $node->id_node ."\""; ?>  > 
 		    <div class="panel-heading">
 		      <h3 class="panel-title"><?php echo "" . $node->description; ?>
@@ -99,7 +99,7 @@ function createPanel($node, $i){
 					<i class="glyphicon glyphicon-info-sign" data-toggle="dropdown"></i>
 		      		<ul class="dropdown-menu" style="background-color: <?php echo $backgroundColor[$i % 6] ?> ">
 		                <li><a href="#">Id node : <?php echo "" . $node->id_node; ?></a></li>
-		                <li><a href="#">Id sur le réseau : <?php echo "" . $node->id; ?></a></li>
+		                <li><a href="#">Id sur le r&eacute;seau : <?php echo "" . $node->id; ?></a></li>
 		                <li><a href="#">Description : <?php echo "" . $node->description; ?></a></li>
 		                <li><a href="#">Sketch name : <?php echo "" . $node->sketch_name; ?></a></li>
 		                <li><a href="#">Count of capteur : <?php echo "" . count($node->capteurs); ?></a></li>
@@ -136,30 +136,27 @@ function createPanel($node, $i){
 
 				  		echo "<br>";
 				  		$valeur = getValCap($node->id_node, $item['childId']);
-				  		echo " ---> <span class=". '"value"' ." > " . $valeur . "</span>";
-						?>
+				  		// echo " ---> <span class=". '"value"' ." > " . $valeur . "</span>";
+				  		?>
+						
 						<?php 
 							if ($item['type'] == 'ACTUATOR'){
 								if ($item['valueType'] == 'ANALOG'){
 									//input SLider et bouton ok
 									?>
-									<script>
-										function changeValeur(e1, e2){
-											e2.val(e1.val());
-										}
-									</script>
-									<input type="text" value=<?php echo '"'. $valeur . '"' ?> style="width: 30px" id=<?php echo '"text_' . $node->id_node . '_' . $item['childId'] . '"'  ?> > 
+									<input type="text" style="text-align: center" class="form-control" value=<?php echo '"'. $valeur . '"' ?> id=<?php echo '"text_' . $node->id_node . '_' . $item['childId'] . '"'  ?> > 
 									<input type="range" value=<?php echo '"'. $valeur . '"' ?> id=<?php echo '"range_' . $node->id_node . '_' . $item['childId'] . '"'; ?> >
-									<button class=<?php echo '"' .$classButton[$i % 6] . ' actuator_btn sam"' ?> id=<?php echo '"btn_'. $node->id_node . '_'. $item['childId'] . '"'?>> OK </button>
+									<button class=<?php echo '"' .$classButton[$i % 6] . ' actuator_btn btn-block"' ?> id=<?php echo '"btn_'. $node->id_node . '_'. $item['childId'] . '"'?>> OK </button>
+									
 
 									<?php
 								}
-								else {
+								else if ($item['valueType'] == 'NUMERIC') {
 									//Boutons on/off
 									?>
-									<div class="onoffswitch">
+									<div class="onoffswitch" style="width: 40%; margin: auto;">
 										<?php $id = '"switch_'. $node->id_node . '_'. $item['childId'] . '"'?>
-									    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox actuator_switch" id=<?php echo "$id"; ?> checked>
+									    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox actuator_switch" id=<?php echo "$id"; ?> <?php echo "" . ($valeur == "1" ? "checked" : ""); ?>>
 									    <label class="onoffswitch-label" for=<?php echo "$id"; ?>>
 									        <span class="onoffswitch-inner"></span>
 									        <span class="onoffswitch-switch"></span>
@@ -168,8 +165,10 @@ function createPanel($node, $i){
 									<?php
 								}
 							}
-							else {
-
+							else if ($item['type'] == 'SENSOR'){
+								?>
+								<input class="form-control value" type="text" value=<?php echo '"' . $valeur .'"'; ?> id=<?php echo '"text_' . $node->id_node . '_' . $item['childId'] . '"'  ?> style="text-align: center">
+								<?php
 							}
 
 						?>
