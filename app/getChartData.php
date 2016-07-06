07640d96c4a95bf62771e22fe1cbd69ec1c8df5e
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-06 19:08:08 +0300

    Add : Variable id_node, childId send by post method. Fetch the min and max value of the sensor in db

--- app/getChartData.php
+++ app/getChartData.php
@@ -2,12 +2,23 @@
     // $id_node = $_POST['id_node'];
     require 'Database.php';
 
-    $sql = 'SELECT payload,receivedAt FROM message WHERE id_node=6 AND childId=0 ORDER BY message.id_message DESC LIMIT 10';
+    $id_node = $_POST['id_node'];
+    $childId = $_POST['childId'];
+
+    $sql = 'SELECT payload,receivedAt FROM message WHERE id_node=' . $id_node .' AND childId=' . $childId. ' AND NOT payload = \'nan\'  ORDER BY message.id_message DESC LIMIT 60';
 
     $db = new Database();
-        
+    
     $result = $db->query($sql);
-    $json = json_encode( $result );
+
+    $ret_val = array();
+    $ret_val['messages'] = $result;
+
+
+    $result = $db->query("SELECT MIN(message.payload) AS min, MAX(message.payload) AS max FROM message WHERE id_node=$id_node AND childId=$childId AND NOT payload = 'nan'ORDER BY message.id_message DESC LIMIT 60");
+    $ret_val['infos'] = $result;
+
+    $json = json_encode( $ret_val );
 
     // echo the json string
     
