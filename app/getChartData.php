e7648f0454d4157c2da478942d6b892c92dee5e5
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-17 10:37:17 +0300

    Don't select bad payload, get min and max from the fetched data only

--- app/getChartData.php
+++ app/getChartData.php
@@ -5,7 +5,7 @@
     $id_node = $_POST['id_node'];
     $childId = $_POST['childId'];
 
-    $sql = 'SELECT payload,receivedAt FROM message WHERE id_node=' . $id_node .' AND childId=' . $childId. ' AND NOT payload = \'nan\'  ORDER BY message.id_message DESC LIMIT 60';
+    $sql = 'SELECT payload,receivedAt FROM message WHERE id_node=' . $id_node .' AND childId=' . $childId. ' AND NOT payload = \'nan\'  AND NOT payload LIKE \'%\\x%\' ORDER BY message.id_message DESC LIMIT 60';
 
     $db = new Database();
     
@@ -15,7 +15,8 @@
     $ret_val['messages'] = $result;
 
 
-    $result = $db->query("SELECT MIN(message.payload) AS min, MAX(message.payload) AS max FROM message WHERE id_node=$id_node AND childId=$childId AND NOT payload = 'nan'ORDER BY message.id_message DESC LIMIT 60");
+    /*$result = $db->query("SELECT MIN(message.payload) AS min, MAX(message.payload) AS max FROM message WHERE id_node=$id_node AND childId=$childId AND NOT payload = 'nan' AND NOT payload LIKE '%\x%' ORDER BY message.id_message DESC");*/
+    $result = $db->query("SELECT MIN(message.payload) AS min, MAX(message.payload) AS max FROM message INNER JOIN (SELECT id_message,payload,receivedAt FROM message WHERE id_node=$id_node AND childId=$childId AND NOT payload = 'nan' AND NOT payload LIKE '%\x%' ORDER BY message.id_message DESC LIMIT 60) AS m ON message.id_message = m.id_message WHERE message.id_node=$id_node AND message.childId=$childId AND NOT message.payload = 'nan' AND NOT message.payload LIKE '%\x%'");
     $ret_val['infos'] = $result;
 
     $json = json_encode( $ret_val );
