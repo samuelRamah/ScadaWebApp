9fa0eb40a9bd03ea50a632120d032cf4c57ea5b1
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-17 10:57:27 +0300

    Put Reload button into the default template

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,3 @@
-<div class="page-header">
-    <h1>Data <span class="pull-right"><input class="btn btn-default" type="button" OnClick="javascript:window.location.reload()" value="Reload"></span></h1>
-    <div id="show"></div>
-</div>
 <div class="row" id="nodes">	
 <?php 
 	// require '../app/panel.php';
--- pages/templates/default.php
+++ pages/templates/default.php
@@ -7,8 +7,13 @@
 	<link rel ="stylesheet" type="text/css" href="css/style.css" />
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/slate-bootstrap.min.css">
+
+	<style type="text/css" media="screen">
+
+	</style>
 </head>
-<body>
+<body >
+
 	<header>
 		 <div class="navbar navbar-default navbar-fixed-top">
 	      <div class="container">
@@ -18,6 +23,7 @@
 	            <span class="icon-bar"></span>
 	            <span class="icon-bar"></span>
 	          </button>
+	          <!-- <span > <img src="images/ispm.png" alt="ispm_logo"></span> -->
 	          <a class="navbar-brand" href="#">Supervisory Control and Data Acquisition</a>
 	        </div>
 	        <div class="nav navbar-nav navbar-right navbar-collapse collapse">
@@ -25,8 +31,11 @@
 	            <li class=<?php echo "\"$class[0]\""?>><a href="index.php?p=home" ><i class="glyphicon glyphicon-home">   Home</i></a>
 	            </li>
 	            <li class=<?php echo "\"$class[1]\""?>><a href="index.php?p=evolution"><i class="glyphicon glyphicon-stats">   Evolution</i></a></li>
-	            <li class=<?php echo "\"$class[2]\""?>><a href="index.php?p=debug"><i class="glyphicon glyphicon-hourglass">   Debug</i></a></li>
-	            <li class=<?php echo "\"dropdown $class[3]\"" ?>>
+	            <li class=<?php echo "\"$class[2]\""?>><a href="index.php?p=schedule"><i class="glyphicon glyphicon-tasks">   Schedule</i></a></li>
+	            <li class=<?php echo "\"$class[3]\""?>><a href="index.php?p=sketchbook"><i class="glyphicon glyphicon-grain">   SketchBook</i></a></li>
+	            <li class=<?php echo "\"$class[4]\""?>><a href="index.php?p=debug"><i class="glyphicon glyphicon-console">   Debug</i></a></li>
+	            <!-- <li class=<?php echo "\"$class[4]\""?>><a href="index.php?p=debug"><i class="glyphicon glyphicon-hourglass">   Debug</i></a></li> -->
+	            <li class=<?php echo "\"dropdown $class[5]\"" ?>>
 	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Log out <b class="caret"></b></a>
 	              <ul class="dropdown-menu">
 	                <li><a href="#">Log out</a></li>
@@ -42,6 +51,10 @@
 	<section>
 
 		<div class="container" id="bodyHome">
+			<div class="page-header">
+			    <h1> <img class="profile-img img-circle" src="images/ispm.png" alt="logo_ispm" height="100px" /> Data <span class="pull-right"><input class="btn btn-default" type="button" OnClick="javascript:window.location.reload()" value="Reload"></span></h1>
+			    <div id="show"></div>
+			</div>
 
 			<?= $content; ?>
 
@@ -50,7 +63,7 @@
 	</section>
 
 	
-	<footer>
+	<footer class="footer">
 		
 	</footer>
 </body>
