077be9c99a326b7677b8feadf298940067e05c4a
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-17 10:55:41 +0300

    Add Min and Max input/choice

--- pages/evolution.php
+++ pages/evolution.php
@@ -1,7 +1,3 @@
-<div class="page-header">
-    <h1>Data <span class="pull-right"><input class="btn btn-default" type="button" OnClick="javascript:window.location.reload()" value="Reload"></span></h1>
-    <div id="show"></div>
-</div>
 <div class="container">
 	<div class="row">
 		<div class="col-md-9" id="canv" style="height: 80%; width: 100%">
@@ -12,41 +8,53 @@
 	<hr>
 	<div class="row">
 		<form class="form-inline" >
-		  <div class="form-group">
-		    <label for="id_node">Node : </label>
-		    <select class="form-control" id="id_node" name="id_node" placeholder="" style="width: 150px">
-	    	<?php 
-	    		// require '../app/panel.php';
-				$nodes = getNodes();
+		  <fieldset>
+		  	<legend>Chart Options</legend>
+		  	<div class="form-group">
+			    <label for="id_node">Node : </label>
+			    <select class="form-control" id="id_node" name="id_node" placeholder="" style="width: 150px">
+		    	<?php 
+		    		// require '../app/panel.php';
+					$nodes = getNodes();
 
-				for ($i = 0; $i < count($nodes); $i++){
-				?>
-					<option value = <?php echo "\"" . $nodes[$i]->id_node . "\""; ?> > <?php echo "" . $nodes[$i]->description; ?> </option>
-				<?php
-				}
-
-	    	 ?>
-		    </select>
-		  </div>
-		  <div class="form-group">
-		    <label for="id_capteur">Capteur : </label>
-		    <select multiple autofocus class="form-control" id="id_capteur" name="id_capteur" placeholder="" style="width: 150px" size="3">
-			<?php 
-				$cible = 0 ;
-				$capteurs = $nodes[$cible]->capteurs ; 
-			  	if (count($capteurs) > 0) {
-			  		foreach ($capteurs as $item) {
-			  			?>
-						<option value = <?php echo "\"" . $item['childId'] . "\""; ?> > <?php echo $item['description']; ?> </option>
-						<?php
-
+					for ($i = 0; $i < count($nodes); $i++){
+					?>
+						<option value = <?php echo "\"" . $nodes[$i]->id_node . "\""; ?> > <?php echo "" . $nodes[$i]->description; ?> </option>
+					<?php
 					}
-				}
-		 	?>
 
-		    </select>
-		  </div>
-		  <button type="button" class="btn btn-primary" id="see_chart">See Chart</button>
+		    	 ?>
+			    </select>
+			  </div>
+			  <div class="form-group">
+			    <label for="id_capteur">Capteur : </label>
+			    <select multiple autofocus class="form-control" id="id_capteur" name="id_capteur" placeholder="" style="width: 150px" size="3">
+				<?php 
+					$cible = 0 ;
+					$capteurs = $nodes[$cible]->capteurs ; 
+				  	if (count($capteurs) > 0) {
+				  		foreach ($capteurs as $item) {
+				  			?>
+							<option value = <?php echo "\"" . $item['childId'] . "\""; ?> > <?php echo $item['description']; ?> </option>
+							<?php
+
+						}
+					}
+			 	?>
+
+			    </select>
+			  </div>
+			  <div class="form-group">
+					<label for="max_val">Min : </label>
+					<input type="text" class="form-control"  id="min_val" width="10"></input>
+			  </div> 
+
+			  <div class="form-group">
+					<label for="max_val">Max : </label>
+					<input type="text" id="max_val" class="form-control " width="10" ></input>
+			  </div> 
+			  <button type="button" class="btn btn-primary" id="see_chart">See Chart</button>
+		  </fieldset>
 		</form>
 	</div>
 </div>
