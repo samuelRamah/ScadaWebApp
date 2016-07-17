16317b576a2887e2dd20e2aa4326100e990ea85b
RAMAHOLIMIHASO Samuel <ramah.samuel@gmail.com>
2016-07-17 10:54:03 +0300

    Add textarea which display message from MySensors network/protocol

--- pages/debug.php
+++ pages/debug.php
@@ -1,1 +1,13 @@
-<h1>DEBUG</h1>
+<!-- <h1>DEBUG</h1> -->
+
+<div class="container">
+	<textarea name="log" id="log" class="form-control"></textarea>
+	<label class="checkbox-inline">	
+		<input type="checkbox" id="autoscroll" checked="checked"> Autoscroll
+	</label>	
+		
+<!-- 	<label class="checkbox-inline">
+	<input type="checkbox" id="autoscroll" value="autoscroll"> Autoscroll
+</label>
+ -->
+</div>
